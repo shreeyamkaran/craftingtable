21ff1c3e61204a8df4c15a5adc2784e8a407c288
Shreeyam Karan <karanshreeyam@gmail.com>
2025-12-21 16:16:09 +0530

    Bootstrap craftingtable project

---
+++ src/main/java/com/karan/craftingtable/CraftingTableApplication.java
@@ -0,0 +1,13 @@
+package com.karan.craftingtable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CraftingTableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CraftingTableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karan/craftingtable/CraftingTableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karan.craftingtable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CraftingTableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
